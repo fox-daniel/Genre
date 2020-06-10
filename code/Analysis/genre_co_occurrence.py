19c6f2d72f8f291d049ab4cb4e53454e2c2f86e8
Daniel Fox <danielfox@thoughtstoodefinite.com>
2020-06-10 13:52:01 -0400

    co-occurrence script: running with all basic functionality on local machine

--- code/Analysis/genre_co_occurrence.py
+++ code/Analysis/genre_co_occurrence.py
@@ -16,6 +16,9 @@ from functools import partial
 
 import plotly.graph_objects as go
 
+
+st.markdown("# Gender and Genre")
+
 #reference the datetime used to title the data
 now = '2020-05-18-10-06'
 
@@ -50,8 +53,7 @@ data['genrelist']= data['genrelist'].apply(genrelist)
 # import genre labels
 genrelist_df = pd.read_csv('/Users/Daniel/Code/Genre/data/genre_lists/data_ready_for_model/genre_list_{}.csv'.format(now), index_col = 'Unnamed: 0')
 
-st.write('There are {} artists with genre and binary-gender labels in the total data set.'.format(data.shape[0]))
-st.write('There are {} unique genre labels.'.format(genrelist_df.shape[0]))
+
 
 data_male = data[data.gender == 'male']
 data_female = data[data.gender == 'female']
@@ -60,28 +62,31 @@ m = data_male.shape[0]
 f = data_female.shape[0]
 fem = 100*f/(f+m)
 mal = 100*m/(f+m)
-st.write('{} total artists'.format(tot))
-st.write('{} female artists, or {:0.0f}%'.format(f, fem))
-st.write('{} male artists, or {:0.0f}%'.format(m, mal))
+
+st.write(data.shape[0], "artists with genre and binary-gender labels")
+st.write(f, "female artists, or", round(fem,2), "%")
+st.write(m,'male artists, or',round(mal,2), '%')
+st.write('{} unique genre labels.'.format(genrelist_df.shape[0]))
 
 #Count the number of times that a label occurs:
-genre_list_1 = data.genrelist.values.tolist()
-genre_list_1 = [x for y in genre_list_1 for x in y]
-genre_counts = pd.Series(genre_list_1)
-label_value_counts = pd.DataFrame(genre_counts.value_counts())
-label_value_counts.columns = ['Frequency']
-"The frequency of appearance of each genre:"
-label_value_counts
+@st.cache(persist=True)
+def generate_list(data):
+    genre_list_1 = data.genrelist.values.tolist()
+    genre_list_1 = [x for y in genre_list_1 for x in y]
+    genre_counts = pd.Series(genre_list_1)
+    label_value_counts = pd.DataFrame(genre_counts.value_counts())
+    label_value_counts.columns = ['Frequency']
+    genres_list_unique = label_value_counts.index.values.tolist()
+    return label_value_counts, genres_list_unique
+
+label_value_counts, genres_list_unique = generate_list(data)
 
 st.write("The frequency of each genre in descending order")
 label_value_counts
 
-
-st.write("This is the data")
-data
-
 st.title("Co-Occurrences")
 
+@st.cache
 def coocurr(QueryGenre):
     # set genre to query
     QueryGenre = QueryGenre 
@@ -94,13 +99,18 @@ def coocurr(QueryGenre):
     # turn it into a Series
     QueryGenre_CoGenres = pd.Series(QueryGenre_CoGenres)
     # make counts of appearances of the co-genres
-    QueryGenre_CoGenres_counts = QueryGenre_CoGenres.value_counts()
+    QueryGenre_CoGenres_counts = pd.DataFrame(QueryGenre_CoGenres.value_counts(), columns = ['Frequency'])
     # drop the QueryGenre itself
     QueryGenre_CoGenres_counts.drop(QueryGenre, axis = 0, inplace = True )
     #QueryGenre_CoGenres_counts.rename_axis( 'counts', inplace = True)
     QueryGenre_CoGenres_counts.index.name = 'genres'
     return QueryGenre_CoGenres_counts
 
-cooccurrences = coocurr('rap')
+query_genre = st.selectbox('Select a genre and see with which genres it co-occurs.',genres_list_unique)
+
+#query_genre = 'hip_hop'
+
+st.write("The genres that co-occurr with",query_genre,":")
+cooccurrences = coocurr(query_genre)
 
 cooccurrences
