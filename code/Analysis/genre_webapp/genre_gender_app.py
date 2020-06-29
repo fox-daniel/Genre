5c8e28cf1477041d8763c2c1b5cf56ab1c95b70d
Daniel Fox <danielfox@thoughtstoodefinite.com>
2020-06-29 12:57:11 -0400

    updated all previous functionality with better design

--- code/Analysis/genre_webapp/genre_gender_app.py
+++ code/Analysis/genre_webapp/genre_gender_app.py
@@ -78,19 +78,27 @@ def main():
     label_value_counts.columns = ["Genre",'Frequency']
 
 
-    page = st.sidebar.radio("Choose Stats", options = ["Artists","Genres"])
+    page = st.sidebar.radio("Choose Stats", options = ["Artists","Genre Counts", "Co-Occurrences of Genres", "Artists of a Genre", "Genres of an Artist"])
 
     if page == "Artists":
 
-        # summary stats on Artists
-        #a,b,c = data.genrelist_length.mean(), data.genrelist_length.std(), data.genrelist_length.max()
+        
 
         st.write('### Stats on Artists')
         st.write(data.shape[0], "artists with genre and binary-gender labels")
         st.write(f, "female artists, or", round(fem,2), "%")
         st.write(m,'male artists, or',round(mal,2), '%')
 
-    elif page == "Genres":
+        st.write('### Stats on Genre Labels and Artists')
+
+        #summary stats on Artists
+        a,b,c = data.genrelist_length.mean(), data.genrelist_length.std(), data.genrelist_length.max()
+        st.write('Mean number of genre labels per artist: {}.'.format(round(a,2)))
+        st.write('STD of the number of genre labels: {}.'.format(round(b,2)))
+        st.write('Max number of genre labels: {}.'.format(c))
+
+
+    elif page == "Genre Counts":
 
         fig = go.Figure(data=[go.Table(
         header=dict(values=list(label_value_counts.columns),
@@ -105,17 +113,122 @@ def main():
         st.write('### Stats on Genre Labels')
         st.write('{} unique genre labels.'.format(genrelist_df.shape[0]))
         st.write('{} genre labels'.format(label_value_counts.Frequency.sum()))
-        st.write("The frequency of each genre in descending order:")
+        st.write("The frequency of each genre in descending order (scroll through table):")
         st.plotly_chart(fig)
 
-        #st.write('### Stats on Genre Labels and Artists')
-        #st.write('Mean number of genre labels per artist: {}.'.format(round(a,2)))
-        #st.write('STD of the number of genre labels: {}.'.format(round(b,2)))
-        #st.write('Max number of genre labels: {}.'.format(c))
 
-    
 
+    elif page == "Co-Occurrences of Genres":
+
+        st.title("Co-Occurrences")
+
+        @st.cache
+        def coocurr(QueryGenre):
+            # set genre to query
+            QueryGenre = QueryGenre 
+            # select artists whose genre list contains QueryGenre
+            artists_with_QueryGenre = data[data.genrelist.apply(lambda x: True if QueryGenre in x else False)]
+            # create a list of genre lists from all artists that have QueryGenre on their list
+            QueryGenre_CoGenres = artists_with_QueryGenre.genrelist.values.tolist()
+            # flatten
+            QueryGenre_CoGenres = [x for y in QueryGenre_CoGenres for x in y]
+            # turn it into a Series
+            QueryGenre_CoGenres = pd.Series(QueryGenre_CoGenres)
+            # make counts of appearances of the co-genres
+            QueryGenre_CoGenres_counts = pd.DataFrame(QueryGenre_CoGenres.value_counts(), columns = ['Frequency'])
+            # drop the QueryGenre itself
+            QueryGenre_CoGenres_counts.drop(QueryGenre, axis = 0, inplace = True )
+            #QueryGenre_CoGenres_counts.rename_axis( 'counts', inplace = True)
+            QueryGenre_CoGenres_counts.index.name = 'Genres'
+            QueryGenre_CoGenres_counts.sort_index(inplace = True)
+            QueryGenre_CoGenres_counts.reset_index(inplace = True)
+            return QueryGenre_CoGenres_counts
+
+        #list_sorted = sorted(genres_list_unique)
+
+        # alphabetize genres
+        genres_alphabetical = sorted(genres_list_unique)
+
+        query_genre = st.selectbox('Select a genre and see with which genres it co-occurs.', genres_alphabetical)
+
+        #query_genre = 'hip_hop'
+
+        st.write("The genres that co-occurr with",query_genre,":")
+        cooccurrences = coocurr(query_genre)
+
+        fig = go.Figure(data=[go.Table(
+        header=dict(values=list(cooccurrences.columns),
+                    fill_color='paleturquoise',
+                    align='left'),
+        cells=dict(values=[cooccurrences['{}'.format(x)] for x in cooccurrences.columns],
+                   fill_color='lavender',
+                   align='left'))
+        ])
+
+        st.plotly_chart(fig)
     
+    elif page == "Artists of a Genre":
+        st.write("Select a genre to see which artists in the data set have been assigned that genre label on Wikipedia.")
+
+
+        def artists_with_label(row, label = 'soul'):
+            if label in row.genrelist:
+                return True
+            else:
+                return False
+
+        # alphabetize genres
+        genres_alphabetical = sorted(genres_list_unique)
+
+        def genre_artists(data, label = 'soul'):
+            artists_with = partial(artists_with_label,label = label) # create the partial function for the selected genre
+            data[label] = data.apply(artists_with, axis = 1) # select those artists with the selected genre
+            artists = data[data[label]].reset_index()
+            artists = pd.DataFrame(artists.artist.sort_values())
+            artists.reset_index(inplace = True, drop = True)
+            artists.columns = ["Artists labeled with {}".format(label)]
+            return artists # produce alphabetical list of artists with the selected genre
+
+
+        query_genre_artist = st.selectbox('Find the artists in a genre.', genres_alphabetical)
+        queried_genre_artists = genre_artists(data, query_genre_artist)
+
+        # st.plotly_chart(queried_genre_artists.values)
+
+        fig = go.Figure(data=[go.Table(
+        header=dict(values=list(queried_genre_artists.columns),
+                    fill_color='paleturquoise',
+                    align='left'),
+        cells=dict(values=[queried_genre_artists['{}'.format(x)] for x in queried_genre_artists.columns],
+                   fill_color='lavender',
+                   align='left'))
+        ])
+
+        st.plotly_chart(fig)
+
+    elif page == "Genres of an Artist":
+        st.write("Select an Artist to see their genres:")
+        def genres_of_an_artist(data, artist_name = 'La_Palabra'):
+            genres = data.loc[artist_name, 'genrelist']
+            genres = ", ".join(map(str,genres))
+            return genres.title()
+
+        artist_name = st.selectbox('Select an artist to see their genres',data.sort_index().index.values.tolist())
+        genres_of_artist = genres_of_an_artist(data, artist_name)
+        #genres_of_artist = pd.DataFrame({'Genres':genres_of_artist})
+        st.write(genres_of_artist)
+
+        # or as a dataframe (still needs str -> list for genres of artist)
+        # fig = go.Figure(data=[go.Table(
+        # header=dict(values=list(genres_of_artist.columns),
+        #             fill_color='paleturquoise',
+        #             align='left'),
+        # cells=dict(values=[genres_of_artist['{}'.format(x)] for x in genres_of_artist.columns],
+        #            fill_color='lavender',
+        #            align='left'))
+        # ])
+
+        # st.plotly_chart(fig)
 
 main()
 # if want to combine with other scripts
